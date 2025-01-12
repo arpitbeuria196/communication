cdb22c31c14b5e4bcab33e624e659113330a4411
arpitbeuria196 <arpitbeuria196@gmail.com>
2025-01-12 21:44:42 +0000

    Output

---
+++ JsInterview/script2.js
@@ -0,0 +1,74 @@
+// function abc()
+// {
+//     console.log(abc.xyz);
+// }
+
+// abc(); //undefined
+// abc.xyz=400
+// abc.xyz=200
+// abc(); //200
+
+// const numbers = [1,2,3,4]
+
+// numbers[100] = 500
+// console.log(numbers);
+
+// console.log(typeof typeof 100); //String
+
+// console.log(typeof String)
+
+// function abc()
+// {
+
+// }
+
+// console.log(typeof abc)
+
+// const arr = [..."Arpit","Amrit"]
+
+// console.log(arr);
+
+// console.log(parseInt('10+2')); // 10
+// console.log(parseInt('7FM')); //7
+// console.log(parseInt('M7F'));//NAN
+
+// console.log(parseInt('Arpit'));
+
+// console.log(isNaN('Praveen'));
+
+// console.log([1,2].map(num=>{
+//     if(num>0) return;
+
+//     return num*2
+// }));
+
+// function abc()
+// {
+//     return;
+// }
+
+// console.log(abc())
+
+// {
+// function abc()
+// {
+//     console.log('Praveen')
+// }
+// }
+
+// abc();
+
+function abc(a,b)
+{
+    'use strict'//variable declared any kind of keywords
+
+    a=100
+    b=200
+
+    return arguments[0] + arguments[1]
+    console.log(a+b); //won't print
+}
+
+console.log(abc(300,400)); // 100,200
+
+
