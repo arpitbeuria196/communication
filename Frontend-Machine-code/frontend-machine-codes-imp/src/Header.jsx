18750e9d182c0f406aece481482f7f0cbc6503dc
arpitbeuria196 <arpitbeuria196@gmail.com>
2025-03-03 20:49:19 +0000

    HeaderAdjust

--- Frontend-Machine-code/frontend-machine-codes-imp/src/Header.jsx
+++ Frontend-Machine-code/frontend-machine-codes-imp/src/Header.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='text-xl font-bold py-5 bg-black text-whit text-center flex'>
-      <nav>
-        <a href='/customModal'>Custom Modal</a>
-        <a href='/starRating'>Star Rating</a>
+      <nav >
+        <a
+        className='p-4'
+        href='/customModal'>CustomModal </a>
+        <a 
+        className='p-4'
+        href='/starRating'>StarRating </a>
       </nav>
     </div>
   )
